bd2bf55e4fe069aceea75047c0edf2af81154b84
david <juliandmarias@gmail.com>
2019-11-14 07:32:11 -0500

    inicio repo

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,31 @@
+import { NgModule, Component } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { ListarClienteComponent } from './Client/list-client/list-client.component';
+import { AddClienteComponent } from './Client/add-client/add-client.component';
+import { EditClienteComponent } from './Client/edit-client/edit-client.component';
+import {DetailClientComponent} from './Client/detail-client/detail-client.component';
+import { ListarReservacionesComponent } from './Reservation/list-reservation/list-reservation.component';
+import { AddReservacionesComponent } from './Reservation/add-reservation/add-reservation.component';
+import {EditReservacionesComponent} from './Reservation/edit-reservation/edit-reservation.component';
+
+
+const routes: Routes = [
+  {path:'listarCliente',component:ListarClienteComponent},
+  {path:'addCliente',component:AddClienteComponent},
+  {path:'editCliente',component:EditClienteComponent},
+  {path:'detailClient', component:DetailClientComponent},
+
+  {path:'listarReservaciones',component:ListarReservacionesComponent},
+  {path:'addReservaciones',component:AddReservacionesComponent},
+ {path:'editReservaciones', component:EditReservacionesComponent}
+  
+ 
+
+  
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
